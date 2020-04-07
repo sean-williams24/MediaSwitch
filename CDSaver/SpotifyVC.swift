c92c73793da795d8ce2d2db22228db7a3ad50389
Sean-Williamsgit config --global user.email sean.r.williams85@gmail.comgit config --global user.name Sean-Williams <wilmslo@gmail.com>
2020-04-07 08:41:42 +0100

    feat: Add Scaled Element processor VCs

---
+++ CDSaver/SpotifyVC.swift
@@ -0,0 +1,25 @@
+//
+//  SpotifyVC.swift
+//  CDSaver
+//
+//  Created by Sean Williams on 06/04/2020.
+//  Copyright © 2020 Sean Williams. All rights reserved.
+//
+
+import UIKit
+
+class SpotifyVC: UIViewController {
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func connectToSpotifyTapped(_ sender: Any) {
+        
+    }
+}
+
--- CDSaver/ViewController.swift
+++ CDSaver/ViewController.swift
@@ -2,24 +2,68 @@
 //  ViewController.swift
 //  CDSaver
 //
-//  Created by Sean Williams on 06/04/2020.
+//  Created by Sean Williams on 12/02/2020.
 //  Copyright © 2020 Sean Williams. All rights reserved.
 //
 
+import Firebase
 import UIKit
 
 class ViewController: UIViewController {
     
-    
+    // MARK: - Outlets
 
+    @IBOutlet var imageView: UIImageView!
+    
+    
+    // MARK: - Properties
+    
+    let processor = ScaledElementProcessor()
+    
+    
+    
+    
+    // MARK: - Life Cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        processor.process(in: imageView) { (text) in
+            print(text)
+        }
     }
+    
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
 
+    }
+    
+    
+    // MARK: - Location Methods
+    
+    
+    
+    // MARK: - Private Methods
+    
 
-    @IBAction func connectToSpotifyTapped(_ sender: Any) {
+    
+    // MARK: - Navigation
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         
     }
+    
+    
+    // MARK: - Action Methods
+    
+
 }
+
+
+    // MARK: - Extensions
+
 
