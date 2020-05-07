a42eb01ba4ffbb57b8ca0aa50350c425e834c8a2
Sean-Williamsgit config --global user.email sean.r.williams85@gmail.comgit config --global user.name Sean-Williams <wilmslo@gmail.com>
2020-05-07 15:48:02 +0100

    tidy: delete boilerplate and unused code

---
+++ CDSaver/Controller/SettingsTableVC.swift
@@ -0,0 +1,40 @@
+//
+//  SettingsTableVC.swift
+//  CDSaver
+//
+//  Created by Sean Williams on 07/05/2020.
+//  Copyright © 2020 Sean Williams. All rights reserved.
+//
+
+import UIKit
+
+class SettingsTableVC: UITableViewController {
+    
+    var selectedOption = ""
+    
+    
+    // MARK: - Table view data source
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        switch indexPath.row {
+        case 0:
+            selectedOption = "terms"
+        case 1:
+            selectedOption = "privacy"
+        case 2:
+            selectedOption = "legal"
+        case 3:
+            selectedOption = "contact"
+        default:
+            break
+        }
+        
+        let vc = storyboard?.instantiateViewController(identifier: "SettingsDetailVC") as! SettingsDetailVC
+        vc.selectedSettingsOption = selectedOption
+        show(vc, sender: self)
+        
+    }
+}
+
+
+
